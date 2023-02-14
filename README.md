dbb8669fa2f25f0319fa82ec824fe45a5b5df3ab
Aloy <122377449+GirlxCode@users.noreply.github.com>
2023-02-13 21:24:07 -0600

    read-me file completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # hw3password-generator
+
+# Description
+
+ In homework assignment number three, we were tasked with creating a random password generator. Our assignment stated that we needed to make sure that the user is able to choose the length of their password, and whether or not to add letters, number, or special characters into the password. Once the password choices had been selected, the user would then be able to see the password on the screen.
+
+ ![](2023-02-13-21-18-43.png)
+
+
+# Links
+ https://girlxcode.github.io/hw3password-generator/
+
+ https://github.com/GirlxCode/hw3password-generator
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,71 +7,58 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // prompts the user to detemermine what kind of passwoord they want
+function generatePassword() {
+    var characterAmount = prompt("How many characters long would you like your password to be? Please enter number between 8 and 128");
+    while((characterAmount<8) || (characterAmount > 128) || (isNaN (characterAmount)=== true)){
+     alert("Please choose a number between 8 and 128");
+    characterAmount = prompt("How many characters long would you like your password to be? Please enter number between 8 and 128");
+    }
+    
+    var chooseLower = confirm("Would you like lowercase letters in your password?");
+    var chooseUpper = confirm("Would you like uppercase letters in your password?");
+    var chooseNumber = confirm("Would you like numbers in your password?");
+    var chooseSpecialCharacter = confirm("Would you like special characters in your password?");
 
-function generatePassword(){
-    var characterAmount = prompt("How many characters long would you like your password to be? Please enter number between 8 and 128")
-    while((characterLimit < 8) (characterLimit > 128) (isNaN) (characterLimit)=== true)
-    alert("Please choose a number between 8 and 128")
-    characterAmount = prompt("How many characters long would you like your password to be? Please enter number between 8 and 128")
+    // all user choices for their password are listed here
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
+    var specialChara = "!#$%&'()/?@[\]";
+    var charaBank = "";
+    var newPassword = "";
+
+    // user choices are held by if statements
+    if (chooseLower === true) {
+        charaBank = charaBank.concat(lowercase);
+    }
+
+    if (chooseUpper === true) {
+        charaBank = charaBank.concat(uppercase);
+    }
+
+    if (chooseNumber === true) {
+        charaBank = charaBank.concat(numbers);
+    }
+
+    if (chooseSpecialCharacter === true) {
+        charaBank = charaBank.concat(specialChara);
+    }
+
+    // allows functions to randomize
+    function randomize(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    // the character bank is looped through and is sent back to the user
+    for (var i=0; i<characterAmount; i++) {
+        newPassword = newPassword.concat(charaBank.charAt(randomize(charaBank.length-1)));
+    }
+
+    return newPassword;
 }
-
-var chooseLower = confirm("Would you like lowercase letters in your password?")
-
-var chooseUpper = confirm("Would you like uppercase letters in your password?")
-
-var chooseNumber = confirm("Would you like numbers in your password?")
-
-var chooseSpecialCharacter = confirm("Would you like special characters in your password?")
-
-// all user choices for their password are listed here
-var lowercase = "abcdefghijklmnopqrstuvwxyz"
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "0123456789"
-var specialChara ="!#$%&'()/?@[\]"
-var charaBank = ""
-var newPassword = ""
-
-// user choices are held by if statements
-
-if (chooseLower === true) {
-    charaBank = charaBank.concat(lowercase)
-}
-
-if (chooseUpper === true) {
-    charaBank = charaBank.concat(uppercase)
-}
-
-if (chooseNumber === true) {
-    charaBank = charaBank.concat(numbers)
-}
-
-if (chooseSpecialCharacter === true) {
-    charaBank = charaBank.concat(specialChara)
-}
-
-// allows functions to randomize
-
-function randomize(max){
-    return [Math.floor(Math.random() * max)]
-}
-
-// the character bank is looped through and is sent back to the user
-
-for (var i=0; i<characterAmount; i++){
-    newPassword = newPassword.concat(charaBank.charAt(randomize(charaBank.length-1)))
-}
-
-return newPassword
-
-
-
-
-
-
